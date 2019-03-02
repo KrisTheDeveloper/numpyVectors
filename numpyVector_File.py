ed2ae8342f180070d66a26e31ee8cd387b6275fb
Kris <kswhite609@hotmail.com>
2019-03-02 15:21:08 -0500

    updated

--- numpyVector_File.py
+++ numpyVector_File.py
@@ -1,17 +1,17 @@
 import numpy as np
 
-#by only putting in a single number it creates a (1,5) row vector vector
-#Don't use this single number initialization
+# by only putting in a single number it creates a (1,5) row vector vector
+# Don't use this single number initialization
 a = np.random.randn(5)
 print(a)
 print(a.shape)
 print(a.T)
 
-print(np.dot(a,a.T))
+print(np.dot(a, a.T))
 
-#this creates a (5,1) column vector
-a=np.random.randn(5,1)
+# this creates a (5,1) column vector
+a = np.random.randn(5, 1)
 print(a)
 print(a.T)
 
-#this should be added to github please
+# this should be added to github please
